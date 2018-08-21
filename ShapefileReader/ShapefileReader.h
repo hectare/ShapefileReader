4c933dbc8205738e209562f7b1367b4c9b62fd36
fanquake <fanquake@gmail.com>
2018-08-21 13:26:51 +0800

    Add ShapefileReader framework target
    This includes all changes to .pbxproj since the moves began..

---
+++ ShapefileReader/ShapefileReader.h
@@ -0,0 +1,18 @@
+//
+//  ShapefileReader.h
+//  ShapefileReader
+//
+//  Copyright © 2018 Hectare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShapefileReader.
+FOUNDATION_EXPORT double ShapefileReaderVersionNumber;
+
+//! Project version string for ShapefileReader.
+FOUNDATION_EXPORT const unsigned char ShapefileReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShapefileReader/PublicHeader.h>
+
+
